6d30585ee2ec6dc0041092fb3f79b435e9d46a3f
dgusfr <diegogustavo.fr@gmail.com>
2024-07-09 09:26:46 -0300

    changes

--- test/user.test.js
+++ test/user.test.js
@@ -1,1 +1,7 @@
-describe("User registration");
+let app = require("../src/app");
+let supertest = require("supertest");
+let request = supertest(app);
+
+describe("User registration", () => {
+  test("You must register a user successfully", () => {});
+});
