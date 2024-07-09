740f828493a72c69fe4a29d4ad8c3c8e019ecbff
dgusfr <diegogustavo.fr@gmail.com>
2024-07-09 09:30:49 -0300

    changes

--- test/user.test.js
+++ test/user.test.js
@@ -4,6 +4,11 @@ let request = supertest(app);
 
 describe("User registration", () => {
   test("You must register a user successfully", () => {
+    //alteração para a cada execução gerar um email unico
+    let time = Date.now();
+    let email = `${time}@gmail.com`;
     let user = { name: "Victor", email: "email@email.com", password: "123456" };
+
+    request.post("/user").send();
   });
 });
