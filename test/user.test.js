ffe8bf6b697e72278a087d796c870058a30fba5c
dgusfr <diegogustavo.fr@gmail.com>
2024-07-09 09:27:58 -0300

    add teste

--- test/user.test.js
+++ test/user.test.js
@@ -3,5 +3,7 @@ let supertest = require("supertest");
 let request = supertest(app);
 
 describe("User registration", () => {
-  test("You must register a user successfully", () => {});
+  test("You must register a user successfully", () => {
+    let user = { name: "Victor", email: "email@email.com", password: "123456" };
+  });
 });
